b75117d90b376467566e19943ead7fbca50646aa
Camila <up202305458@up.pt>
2026-04-02 20:38:20 +0100

    fixed error with using set in bfs

--- Robot.h
+++ Robot.h
@@ -13,7 +13,9 @@ public:
   Position getPos();
   void setPos(Position pos);
   void setColor(Color c);
-  void draw(sf::RenderWindow& window, float cellSize, float offset);
+  void draw(sf::RenderWindow &window, float cellSize, float offset);
+
+  bool operator!=(const Robot &r) const { return !(pos == r.pos); }
 };
 
 #endif
--- State.h
+++ State.h
@@ -17,7 +17,12 @@ public:
   State(Board board, Robot redRobot, Robot greenRobot, Robot blueRobot,
         Robot yellowRobot);
   Board getBoard();
-  Robot& getRobot(Color color);
+  Robot &getRobot(Color color);
+
+  bool operator<(const State &s) const {
+    return redRobot != s.redRobot && greenRobot != s.greenRobot &&
+           blueRobot != s.blueRobot && yellowRobot != s.yellowRobot;
+  }
 };
 
 #endif
